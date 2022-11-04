96f4c8f33761124d96e2f90d747cee83e643d875
Dennis Ekner <deekner@hotmail.com>
2022-11-04 16:24:20 +0100

    Add project files.

---
+++ Labb 1 – Grunderna i OOP/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_1___Grunderna_i_OOP
+{
+    public class Circle
+    {
+        float _pi = 3.141f;
+        int _Radius;
+
+        public Circle(int Radius)
+        {
+            _Radius = Radius;
+        }
+        public float GetArea()
+        {
+            return _Radius * _Radius * _pi;
+        }
+    }
+
+
+
+    
+
+}
---
+++ Labb 1 – Grunderna i OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Labb_1___Grunderna_i_OOP
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Namn: Dennis Ekner Klass: SUT22
+
+            Circle sum5 = new Circle(5);
+            sum5.GetArea();
+            Console.WriteLine("Circle with radius 5: {0}", sum5.GetArea());
+
+            Circle sum6 = new Circle(6);
+            sum6.GetArea();
+            Console.WriteLine("Circle with radius 6: {0}", sum6.GetArea());
+
+
+        }
+    }
+}
